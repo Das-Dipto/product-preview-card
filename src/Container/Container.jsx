ea0623d2070088eb6b2d6169b8d4874bad4347ae
Dipto Das <ddipto396@gmail.com>
2023-04-02 23:48:51 +0600

    Project is completed and ready for deployment

--- src/Container/Container.jsx
+++ src/Container/Container.jsx
@@ -5,11 +5,11 @@ import Content from '../Container/Content'
 
 function Container() {
   return (
-    <div className="container grid grid-cols-1 md:grid-cols-2 py-8 mt-20 md:mt-28 w-[80%] mx-auto">
+    <div className="container grid grid-cols-1 md:grid-cols-2 py-8 mt-20 md:mt-16 w-[80%] mx-auto">
         <div className="hero-image">
             <HeroImage/>
         </div>
-        <div className="content bg-[#fff] w-full md:w-[20rem]">
+        <div className="content bg-[#fff] w-full md:w-[20rem] rounded-b-lg md:rounded-none md:rounded-r-lg">
             <Content></Content>
         </div>
     </div>
--- src/Container/HeroImage.jsx
+++ src/Container/HeroImage.jsx
@@ -4,7 +4,7 @@ import heroImage from '../../src/assets/desktop-image.jpg'
 function HeroImage() {
   return (
     <div className='flex justify-end h-56 md:h-full'>
-    <img className='w-full md:w-[20rem] object-cover' src={heroImage} alt="heroImage" />
+       <img className='w-full md:w-[20rem] object-cover rounded-t-lg md:rounded-none md:rounded-l-lg' src={heroImage} alt="heroImage" />
     </div>
   )
 }
